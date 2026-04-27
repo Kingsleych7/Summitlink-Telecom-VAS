8cff749aab2de2411200c7aa16a307512975c66c
KINGSLEY CHIGBOGU OKAFOR <169157162+Kingsleych7@users.noreply.github.com>
2026-04-27 03:07:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-=======
-const axios = require("axios");
-const crypto = require("crypto");
-
-const redis = require("./src/config/redis");
-const PORT = process.env.PORT || 10000;
-
-const { getRequestId } = require("./src/utils/idempotency");
-
-const airtimeQueue = require("./src/services/queue");
-
-const sendSMS = require("./src/services/sms");
->>>>>>> e47d5ba (updated USSD controller and fixes)
 
 const ussdController = require("./src/ussd/ussdController");
 
@@ -27,25 +13,12 @@ app.use(express.json());
 // USSD route
 app.post("/ussd", ussdController);
 
-<<<<<<< HEAD
-// DB connection
-=======
-// =======================
-// MOCK FUNCTIONS
-// =======================}
-
-async function buyData(phone, plan) {
-    return true;
-}
-
-// =======================
-// MONGODB
-// =======================
+// DB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("DB connected"))
 .catch(err => console.log("DB error:", err));
 
-// Start server
+// SERVER
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
